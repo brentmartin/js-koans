c1e8c6962b9f20cca89e6f7f39ffbdb0d7af1ca1
Brent Martin <bmart35@gmail.com>
2016-09-12 17:04:25 -0500

    finished "should confirm objects are collections of properties" - in objects

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -8,7 +8,7 @@ describe("About Objects", function () {
     });
 
     it("should confirm objects are collections of properties", function () {
-      expect(megalomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(megalomaniac.mastermind).toBe([{"Joker", "Harley"}]);
     });
 
     it("should confirm that properties are case sensitive", function () {
